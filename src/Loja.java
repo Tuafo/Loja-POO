4e6c4f22c30633a299ac9350a505b1cb6986e01c
lubragon <lubragon@gmail.com>
2022-10-19 16:43:49 -0300

    Criando Repositório

---
+++ src/Loja.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Loja {
+
+    private ArrayList<Pedido> listaPedidos = new ArrayList<Pedido>();
+    int p[] = new int[1000];
+    Map<String, String> valores = new HashMap<String, String>();
+
+
+    public Loja() {
+        this.listaPedidos = listaPedidos;
+    
+    }
+    public void criarPedido(){
+        Pedido p1 = new Pedido(1, InOut.leString("Digite seu nome: "));
+    
+
+        
+    }
+
+}
